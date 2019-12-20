c679a0fb385722ac304c211e437234f10eecbd24
Trevor O'Farrell <47125174+trevor-ofarrell@users.noreply.github.com>
2019-12-20 14:17:46 -0800

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -35,6 +35,8 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+            if not self.id:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.utcnow()
