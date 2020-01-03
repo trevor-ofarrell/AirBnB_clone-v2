9f9f5142d4810b794ec59d67bf166e9c02362616
trevor-ofarrell <744@holbertonschool.com>
2020-01-03 23:20:02 +0000

    shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,9 +13,10 @@ apt-get -y update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo $testfile >> /data/web_static/releases/test/index.html
 ln -sfn /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu.ubuntu /data/
-echo $data >> /etc/nginx/nginx.conf
+echo "$code" >> /etc/nginx/nginx.conf
 service nginx restart
