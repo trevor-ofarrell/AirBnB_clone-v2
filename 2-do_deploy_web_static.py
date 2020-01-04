3cca4fde0c53b5bcd9d07c6e4488d63a853579ad
trevor-ofarrell <744@holbertonschool.com>
2020-01-04 04:55:35 +0000

    refactor

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,6 @@
 """Write a Fabric script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web
 servers, using the function do_deploy"""
-from datetime import datetime
 from fabric.api import *
 from os import path
 
@@ -15,19 +14,24 @@ def do_deploy(archive_path):
     ap = new_path = archive_path.split("/")[1]
     if not path.exists(archive_path):
         return False
-    print(ap)
-    put(archive_path, '/tmp/')
-    run("mkdir -p /data/web_static/releases/{:s}".format(
-        new_path))
-    run("tar -xzf /tmp/{:s} -C /data/web_static/releases/{:s}".format(
-        ap, new_path))
-    run("rm /data/web_static/current")
-    run(
-        "mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}"
-        .format(new_path, new_path))
-    run("rm -rf /data/web_static/releases/{:s}/web_static".format(
-        new_path))
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{:s} /data/web_static/current"
-        .format(new_path))
-    return True
+
+    try:
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{:s}".format(
+            new_path))
+        run("tar -xzf /tmp/{:s} -C /data/web_static/releases/{:s}".format(
+            ap, new_path))
+        run("rm /data/web_static/current")
+        run(
+            "mv /data/web_static/releases/{}/web_static/* " +
+            "/data/web_static/releases/{}"
+            .format(new_path, new_path))
+        run("rm -rf /data/web_static/releases/{:s}/web_static".format(
+            new_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{:s} /data/web_static/current"
+            .format(new_path))
+        print("New version deployed!")
+        return True
+    except:
+        return False
