7bf64fb73e6461ffa29750d55e6a4c04ebfa8fe1
trevor-ofarrell <744@holbertonschool.com>
2020-01-04 04:29:36 +0000

    fixing path spliting

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,25 +10,21 @@ env.hosts = ['104.196.135.236 web-01', '35.237.31.37 web-02']
 
 
 def do_deploy(archive_path):
-    new_path = archive_path.split(".")
+    new_path = archive_path.split("/")[1].split(".")[0]
     if not path.exists(archive_path):
         return False
-    try:
-        put('versions/archive_path', '/tmp/archive_path')
-        run("mkdir -p /data/web_static/releases/{:s}".format(
-            new_path))
-        run("tar -xzf /tmp/{:s} -C /data/web_static/releases/{:s}".format(
-            archive_path, new_path))
-        run("rm /data/web_static/current")
-        run(
-            "mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}"
-            .format(new_path, new_path))
-        run("rm -rf /data/web_static/releases/{:s}/web_static".format(
-            new_path))
-        run("rm -rf /data/web_static/current")
-        run(
-            "ln -s /data/web_static/releases/{:s} /data/web_static/current"
-            .format(new_path))
-        return True
-    except:
-        return False
+    print(new_path)
+    put('versions/archive_path', '/tmp/archive_path')
+    run("mkdir -p /data/web_static/releases/{:s}".format(
+        new_path))
+    run("tar -xzf /tmp/{:s} -C /data/web_static/releases/{:s}".format(
+        archive_path, new_path))
+    run("rm /data/web_static/current")
+    run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}"
+        .format(new_path, new_path))
+    run("rm -rf /data/web_static/releases/{:s}/web_static".format(
+        new_path))
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/{:s} /data/web_static/current"
+        .format(new_path))
+    return True
