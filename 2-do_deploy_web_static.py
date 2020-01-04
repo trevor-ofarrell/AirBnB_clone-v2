da9eab59846a56d9dc14aa11eb63ac45079fb176
trevor-ofarrell <744@holbertonschool.com>
2020-01-04 03:33:59 +0000

    adding decompressing script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,9 +13,9 @@ def do_deploy(archive_path):
     if path.exists(archive_path):
         return False
     try:
-        local("tar -xfv" )
+        local("tar -xzf /tmp/{:s} -C /data/web_static/releases/{:s}".format(archive_path, new_path))
         local("rm /data/web_static/current")
-        local("ln -s /data/web_static/releases/new_path /data/web_static/current")
+        local("ln -s /data/web_static/releases/{:s} /data/web_static/current".format(new_path))
         return True
     except:
         return False
