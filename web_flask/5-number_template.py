937bfdbd04828bd41b01ed7f0e9941cd2c94c6d0
trevor-ofarrell <744@holbertonschool.com>
2020-01-22 02:15:06 +0000

    render html template with jinha2

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application: """
+from flask import Flask
+from flask import request, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    text = text.replace("_", " ")
+    return ("C " + text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pyisfun(text="is_cool"):
+    text = text.replace("_", " ")
+    return ("Python " + text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    if type(n) == int:
+        return ("{} {}".format(n, "is a number"))
+    return
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    if type(n) == int:
+        return render_template('5-number.html', n=n)
+    return
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
