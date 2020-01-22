caea9a945c726c6dbfc63aaf6fc256dbea6ea038
trevor-ofarrell <744@holbertonschool.com>
2020-01-22 02:51:42 +0000

    renaming file

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,7 +49,7 @@ def number_odd_or_even(n):
             x = "even"
         else:
             x = "odd"
-        return render_template('6-number.html', n=n, x=x)
+        return render_template('6-number_odd_or_even.html', n=n, x=x)
     return
 
 if __name__ == '__main__':
